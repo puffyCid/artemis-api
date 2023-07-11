6a485212a65d70035360f610dcb5dc902fbdf49d
puffyCid <16283453+puffyCid@users.noreply.github.com>
2023-07-10 21:41:49 -0400

    comments

--- src/linux/elf.ts
+++ src/linux/elf.ts
@@ -6,8 +6,11 @@
  *  - https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
  */
 export interface ElfInfo {
+  /**Array of symbols in ELF binary */
   symbols: string[];
+  /**Array of sections in ELF binary */
   sections: string[];
+  /**Machine type information in ELF binary */
   machine_type: string;
 }
 
