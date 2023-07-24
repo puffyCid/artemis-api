1acaffe955edba8661234fc278749abe65afddfd
puffycid <puffycyan@gmail.com>
2023-07-24 00:33:07 -0400

    spelling

--- src/encoding/strings.ts
+++ src/encoding/strings.ts
@@ -3,7 +3,7 @@
  * @param data Raw bytes to extract UTF8 string from
  * @returns An UTF8 string. Will return empty string if extraction fails
  */
-export function extract_utf8_string(data: Uint8Array): string {
+export function extractUtf8String(data: Uint8Array): string {
   //@ts-ignore: Custom Artemis function
   const result: string = encoding.extract_utf8_string(data);
   return result;
