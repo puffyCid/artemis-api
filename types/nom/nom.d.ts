89b7917060b76c36ec837698baa6392acc7ce430
puffyCid <16283453+puffyCid@users.noreply.github.com>
2023-08-24 01:04:27 -0400

    attempting nom bindings

--- types/nom/nom.d.ts
+++ types/nom/nom.d.ts
@@ -5,5 +5,5 @@ export interface Nom {
   /**Remaining string or bytes provided to nom */
   remaining: string | Uint8Array;
   /**How much string or bytes was nommed */
-  result: string | Uint8Array;
+  nommed: string | Uint8Array;
 }
