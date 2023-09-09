a9794c665bdcce8e02f3edd5538c980291f43fdd
unknown <puffycid@gmail.com>
2023-09-08 21:44:32 -0400

    shortcut api improvements

--- types/windows/shortcuts.d.ts
+++ types/windows/shortcuts.d.ts
@@ -69,6 +69,66 @@ export interface Shortcut {
   birth_droid_file_id: string;
   /**Shellitems associated with shortcut (lnk) file */
   shellitems: ShellItems[];
-  /**Root guid for a property store */
-  property_guid: string;
+  /**Array of property stores */
+  properties: Record<string, unknown>[];
+  /**Environmental variable data in shortcut */
+  environment_variable: string;
+  /**Console metadata in shortcut */
+  console: Console[];
+  /**Windows Codepage in shortcut */
+  codepage: number;
+  /**Special folder ID in shortcut */
+  special_folder_id: number;
+  /**macOS Darwin ID in shortcut */
+  darwin_id: string;
+  /**Shim layer entry in shortcut */
+  shim_layer: string;
+  /**Known folder GUID in shortcut */
+  known_folder: string;
+}
+
+/**
+ * Console metadata embeded in Shortcut file
+ */
+interface Console {
+  /**Colors for Console */
+  color_flags: string[];
+  /**Additional colors for Console */
+  pop_fill_attributes: string[];
+  /**Console width buffer size */
+  screen_width_buffer_size: number;
+  /**Console height buffer size */
+  screen_height_buffer_size: number;
+  /**Console window width */
+  window_width: number;
+  /**Console window height */
+  window_height: number;
+  /**Console X coordinate */
+  window_x_coordinate: number;
+  /**Console Y coordinate */
+  window_y_coordinate: number;
+  /**Console font size */
+  font_size: number;
+  /**Console font family */
+  font_family: string;
+  /**Conesole font weight */
+  font_weight: string;
+  /**Console font name */
+  face_name: string;
+  /**Console cursor size */
+  cursor_size: string;
+  /**Is full screen set (boolean) */
+  full_screen: number;
+  /**Insert mode */
+  insert_mode: number;
+  /**Automatic position set (boolean) */
+  automatic_position: number;
+  /**Console history buffer size */
+  history_buffer_size: number;
+  /**Console number of bufffers */
+  number_history_buffers: number;
+  /**Duplicates allowed in history */
+  duplicates_allowed_history: number;
+  /**Base64 encoded color table. */
+  color_table: string;
 }
