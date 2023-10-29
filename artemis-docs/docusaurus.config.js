08e31d61f25f4d1dccee90755ec076d15cb918f5
puffyCid <16283453+puffyCid@users.noreply.github.com>
2023-10-29 17:47:55 -0400

    updates

--- artemis-docs/docusaurus.config.js
+++ artemis-docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   themes: ["@docusaurus/theme-mermaid"],
 
   // Set the production url of your site here
-  // url: "https://your-docusaurus-test-site.com",
+  url: "",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/artemis-api",
