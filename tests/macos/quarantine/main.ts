36285f12b8b4acbfbcdb9316e099be9dc278e2ae
puffyCid <16283453+puffyCid@users.noreply.github.com>
2024-05-08 20:13:15 -0400

    test workflow

--- tests/macos/quarantine/main.ts
+++ tests/macos/quarantine/main.ts
@@ -1,10 +1,16 @@
-import { quarantineEvents } from "../../../mod.ts";
+import { getAmcache, quarantineEvents } from "../../../mod.ts";
 import { MacosError } from "../../../src/macos/errors.ts";
+import { WindowsError } from "../../../src/windows/errors.ts";
 
 function main() {
   const data = quarantineEvents();
   if (data instanceof MacosError && !data.message.includes("no such table")) {
     throw data;
+  }
+
+  const test = getAmcache();
+  if (test instanceof WindowsError) {
+    throw "this should fail";
   }
 }
 
