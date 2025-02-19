a505328690d47ec260e1e330321cdd5d13d04d41
puffyCid <puffycyan@gmail.com>
2025-02-19 01:22:13 -0800

    updates

--- src/windows/bits.ts
+++ src/windows/bits.ts
@@ -4,34 +4,15 @@ import { WindowsError } from "./errors.ts";
 /**
  * Function to parse default `BITS` location
  * @param carve Whether to carve additional jobs and files
- * @returns `BITS` object containing bits, carved jobs, carved files arrays or `WindowsError`
+ * @param path Optional path to `BITS` file
+ * @returns `Bits` object containing bits, carved jobs, carved files arrays or `WindowsError`
  */
-export function getBits(carve: boolean): Bits | WindowsError {
+export function getBits(carve: boolean, path?:string): Bits | WindowsError {
   try {
     //@ts-ignore: Custom Artemis function
-    const results: string = Deno.core.ops.get_bits(carve);
-
-    const data: Bits = JSON.parse(results);
-    return data;
+    const results = js_bits(carve, path);
+    return results;
   } catch (err) {
     return new WindowsError("BITS", `failed to parse bits: ${err}`);
-  }
-}
-
-/**
- * Function to parse `BITS` file at provided path
- * @param path path to `BITS` file
- * @param carve Whether to carve additional jobs and files
- * @returns `BITS` object containing bits, carved jobs, carved files arrays or `WindowsError`
- */
-export function getBitsPath(path: string, carve: boolean): Bits | WindowsError {
-  try {
-    //@ts-ignore: Custom Artemis function
-    const results: string = Deno.core.ops.get_bits_path(path, carve);
-
-    const data: Bits = JSON.parse(results);
-    return data;
-  } catch (err) {
-    return new WindowsError("BITS", `failed to parse bits at ${path}: ${err}`);
   }
 }
--- src/windows/users.ts
+++ src/windows/users.ts
@@ -3,36 +3,15 @@ import { WindowsError } from "./errors.ts";
 
 /**
  * Function to parse Windows user entries
+ * @param path Optional path to SAM file
  * @returns Array of `UserInfo` entries or `WindowsError`
  */
-export function getUsersWin(): UserInfo[] | WindowsError {
+export function getUsersWin(path?:string): UserInfo[] | WindowsError {
   try {
     //@ts-ignore: Custom Artemis function
-    const data = Deno.core.ops.get_users_windows();
-
-    const results: UserInfo[] = JSON.parse(data);
-    return results;
+    const data = js_users_windows(path);
+    return data;
   } catch (err) {
     return new WindowsError("USERS", `failed to parse users: ${err}`);
-  }
-}
-
-/**
- * Function to parse Windows user entries on an alternative path
- * @param path Full path to SAM file
- * @returns Array of `UserInfo` entries or `WindowsError`
- */
-export function getAltUsersWin(path: string): UserInfo[] | WindowsError {
-  try {
-    //@ts-ignore: Custom Artemis function
-    const data = Deno.core.ops.get_alt_users_windows(path);
-
-    const results: UserInfo[] = JSON.parse(data);
-    return results;
-  } catch (err) {
-    return new WindowsError(
-      "USERS",
-      `failed to parse users at path ${path}: ${err}`,
-    );
   }
 }
