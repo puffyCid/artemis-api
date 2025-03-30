b9bc3af9bbe63651d5ef1c7b5c925fa4df6eaae5
puffycid <puffycyan@gmail.com>
2025-03-29 22:16:27 -0400

    updates

--- tests/windows/compile_tests.bat
+++ tests/windows/compile_tests.bat
@@ -5,7 +5,7 @@ setlocal EnableExtensions DisableDelayedExpansion
 for /D %%i in ("*") do (
   cd %%i 
   echo "Running test for %%i"
-.\node_modules\.bin\esbuild  run -A build.ts
+..\node_modules\.bin\esbuild  run -A build.ts
   ::set value=
   setlocal EnableDelayedExpansion
   ..\script_tester.exe main.js
