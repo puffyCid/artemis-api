f23cd6f8b136d4d0fd2946d5b18f120da55280ba
puffycid <puffycyan@gmail.com>
2025-05-25 03:17:27 -0400

    url fix

--- artemis-docs/docusaurus.config.js
+++ artemis-docs/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: "https://github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/artemis",
+  baseUrl: "/artemis-api",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
